ab3fc4114c829c996472ee65fda81318bc640c08
Luke Bemish <lukebemish@lukebemish.dev>
2024-02-01 16:56:23 -0600

    Fix settings plugin

--- src/main/groovy/dev/lukebemish/conventions/SettingsPlugin.groovy
+++ src/main/groovy/dev/lukebemish/conventions/SettingsPlugin.groovy
@@ -7,6 +7,7 @@ import org.gradle.api.initialization.Settings
 import org.gradle.api.initialization.dsl.VersionCatalogBuilder
 import org.gradle.caching.configuration.BuildCacheConfiguration
 import org.gradle.caching.http.HttpBuildCache
+import org.gradle.caching.http.HttpBuildCacheCredentials
 
 @CompileStatic
 class SettingsPlugin implements Plugin<Settings> {
@@ -16,8 +17,8 @@ class SettingsPlugin implements Plugin<Settings> {
         settings.plugins.apply('com.gradle.enterprise')
 
         settings.dependencyResolutionManagement {
-            versionCatalogs {
-                libs {
+            it.versionCatalogs { container ->
+                container.named('libs') {
                     Versioning.apply(it as VersionCatalogBuilder)
                 }
             }
@@ -26,9 +27,9 @@ class SettingsPlugin implements Plugin<Settings> {
         settings.extensions.getByType(GradleEnterpriseExtension).tap {
             if (System.getenv('CI') != null) {
                 buildScan {
-                    publishAlways()
-                    termsOfServiceUrl = "https://gradle.com/terms-of-service"
-                    termsOfServiceAgree = "yes"
+                    it.publishAlways()
+                    it.termsOfServiceUrl = "https://gradle.com/terms-of-service"
+                    it.termsOfServiceAgree = "yes"
                 }
             }
         }
@@ -36,28 +37,28 @@ class SettingsPlugin implements Plugin<Settings> {
         settings.extensions.getByType(BuildCacheConfiguration).tap {
             if (settings.providers.gradleProperty('buildCacheUrl').orNull) {
                 remote(HttpBuildCache) {
-                    url = settings.providers.gradleProperty('buildCacheUrl').orNull
-                    credentials {
-                        username = settings.providers.gradleProperty('buildCacheUser').orNull
-                        password = settings.providers.gradleProperty('buildCachePassword').orNull
+                    it.url = settings.providers.gradleProperty('buildCacheUrl').orNull
+                    it.credentials { HttpBuildCacheCredentials credentials ->
+                        credentials.username = settings.providers.gradleProperty('buildCacheUser').orNull
+                        credentials.password = settings.providers.gradleProperty('buildCachePassword').orNull
                     }
                     if (settings.providers.gradleProperty('buildCachePush').orNull) {
-                        push = true
+                        it.push = true
                     } else {
-                        push = false
+                        it.push = false
                     }
                 }
             } else if (System.getenv('BUILD_CACHE_URL')) {
                 remote(HttpBuildCache) {
-                    url = System.getenv('BUILD_CACHE_URL')
-                    credentials {
-                        username = System.getenv('BUILD_CACHE_USER')
-                        password = System.getenv('BUILD_CACHE_PASSWORD')
+                    it.url = System.getenv('BUILD_CACHE_URL')
+                    it.credentials { HttpBuildCacheCredentials credentials ->
+                        credentials.username = System.getenv('BUILD_CACHE_USER')
+                        credentials.password = System.getenv('BUILD_CACHE_PASSWORD')
                     }
                     if (System.getenv('BUILD_CACHE_PUSH') || System.getenv('CI')) {
-                        push = true
+                        it.push = true
                     } else {
-                        push = false
+                        it.push = false
                     }
                 }
             }
