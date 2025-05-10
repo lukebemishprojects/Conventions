90b36683209489fc0c03bef50d043e41fb0cfa05
Luke Bemish <lukebemish@lukebemish.dev>
2025-05-10 15:51:34 -0500

    Update to new structure of managed versioning

---
+++ build-convention/src/main/groovy/convention/versioning/VersioningPlugin.groovy
@@ -0,0 +1,62 @@
+package convention.versioning
+
+import dev.lukebemish.managedversioning.ManagedVersioningExtension
+import org.gradle.api.Plugin
+import org.gradle.api.initialization.Settings
+
+class VersioningPlugin implements Plugin<Settings> {
+	@Override
+	void apply(Settings settings) {
+		settings.getPluginManager().apply('dev.lukebemish.managedversioning')
+
+		settings.extensions.getByType(ManagedVersioningExtension).tap {
+			versionFile.set settings.layout.rootDirectory.file('version.properties')
+			gitWorkingDir.set settings.layout.rootDirectory
+			versionPRs()
+			versionSnapshots()
+
+			publishing.mavenPullRequest()
+			publishing.mavenRelease()
+
+			gitHubActions {
+				it.release {
+					it.prettyName.set 'Release'
+					it.workflowDispatch.set(true)
+					it.gradleJob {
+						it.name.set 'build'
+						it.step {
+							it.setupGitUser()
+						}
+						it.buildCache()
+						it.readOnly.set false
+						it.gradlew 'Tag Release', 'tagRelease'
+						it.gradlew 'Build', 'build'
+						it.step {
+							it.run.set 'git push && git push --tags'
+						}
+						it.gradlew 'Publish', 'publish'
+						it.mavenRelease('github')
+					}
+				}
+				it.build_pr {
+					it.prettyName.set 'Build PR'
+					it.pullRequest.set(true)
+					it.gradleJob {
+						it.name.set 'build'
+						it.gradlew 'Build', 'build'
+						it.gradlew 'Publish', 'publish'
+						it.pullRequestArtifact()
+					}
+				}
+				it.publish_pr {
+					it.prettyName.set 'Publish PR'
+					it.publishPullRequestAction(
+						'github',
+						'dev/lukebemish/conventions/conventions-*',
+						'Build PR'
+					)
+				}
+			}
+		}
+	}
+}
